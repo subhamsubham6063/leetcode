34fbff20932fca3d183b30ffbe40909354567130
subhamsubham6063 <132640821+subhamsubham6063@users.noreply.github.com>
2025-11-18 12:10:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (9.68%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSub(string s) {
+        //vector<int>sprints;
+        long long int M=1e9+7;
+        int n =s.size();
+        long long int an=0;
+            long long int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i] == '1'){
+                ans++;
+                an=(an+ ans)%M;
+                }
+            if(s[i] != '1'){
+                ans=0;
+            }
+        }
+      //  ans=0;
+
+        // for(auto &it:sprints){
+        //     cout<<it<<' ';
+        //     ans += (it*(it+1))/2;
+        // }
+
+        return (int)an;
+    }
+};
